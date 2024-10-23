4428093614d9bb795e0d6f0938291b5eba1994bc
Diawel <38136957+diawel@users.noreply.github.com>
2024-10-24 01:18:11 +0900

    fix: stampではなくstickerに変更

--- src/api/stamp/controllers/stamp.ts
+++
@@ -1,7 +0,0 @@
-/**
- * stamp controller
- */
-
-import { factories } from '@strapi/strapi'
-
-export default factories.createCoreController('api::stamp.stamp');
--- src/api/stamp/services/stamp.ts
+++
@@ -1,7 +0,0 @@
-/**
- * stamp service
- */
-
-import { factories } from '@strapi/strapi';
-
-export default factories.createCoreService('api::stamp.stamp');
